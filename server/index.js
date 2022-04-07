173ad21ffee325c22a39a6fb1afb2bcaebf22c8c
Montii <llh9@yahoo.com>
2022-04-07 09:59:46 -0400

    v2

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-
+app.get('/', (req, res) =>{
+  res.send('APP IS RUNNING.');
+});
 const CONNECTION_URL = 'mongodb+srv://landon:password12345@cluster0.um9k3.mongodb.net/memories-db?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
